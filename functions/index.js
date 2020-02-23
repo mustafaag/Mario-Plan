f37a6719d2cbd6a748e9e8c4e1136796734a60e3
mustafaag <mustafaaga23@gmail.com>
2020-02-23 22:06:35 +0100

    Added new cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,47 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+admin.initializeApp(functions.config.firebase);
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+const createNotification = (notification) => {
+    return admin.firestore().collection('notifications')
+    .add(notification)
+    .then(doc =>{
+        console.log('Notification added', doc)
+    })
+}
+
+exports.projectCreation = functions.firestore
+    .document('projects/{projectsId}')
+    .onCreate(doc =>{
+        const project = doc.data();
+        const notification = {
+            content: 'Added e new project',
+            user: `${project.authorFname} ${project.authorLname}`,
+            time: admin.firestore.FieldValue.serverTimestamp()
+        }
+        return createNotification(notification);
+    }
+);
+
+exports.userJoined = functions.auth.user()
+    .onCreate(user =>{
+         admin.firestore().collection('users')
+        .doc(user.uid)
+        .get().then(doc =>{
+            const newUser = doc.data();
+            const notification = {
+                content: 'Joined the party',
+                user: `${newUser.fname} ${newUser.lname}`,
+                time: admin.firestore.FieldValue.serverTimestamp()
+            }
+            return createNotification(notification);
+        })
+    }
+)
+
+
